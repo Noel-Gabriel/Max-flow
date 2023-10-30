15b042f700ed3bff3930bdde0cfaed014c58e6ae
Noël Gabriel De Faveri <117768771+Noel-Gabriel@users.noreply.github.com>
2023-10-30 20:59:48 +0100

    Comment on counter.cpp/.h

--- Max flow/algorithms/counter.cpp
+++ Max flow/algorithms/counter.cpp
@@ -1,3 +1,14 @@
+/**
+ * Not important. These are just counters that count the number of 
+ * times ford-fulkersn, edmonds-karp and dinic's access an edge
+ * during the computation of the bfs/dfs of each algorithm. 
+
+ * Makes for a nice comparison and shows the improvement of dinic's over both
+ * ford-fulkerson and edmonds-karp (and edmonds-karp over ford-fulkerson).
+ * 
+ * Most of the time as expected
+ * ff_edges_visited < ek_edges_visited < dinic_edges_visited
+ */
 namespace C {
     unsigned long long ff_edges_visited = 0;
     unsigned long long ek_edges_visited = 0;
